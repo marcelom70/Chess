7ce9559b1235e2cfef097358d0da5d68a21ef5d1
henriquericcio <henrique.riccio@iconware.com.br>
2013-01-25 20:01:52 -0200

    Adicionando Domain Model

---
+++ src/Chess.Application/Class1.cs
@@ -0,0 +1,36 @@
+﻿using Chess.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Application
+{
+    public class ChessFacade
+    {
+        public Guid SetUpMatch() {
+            var match = new Match(); //cria um novo match
+            
+            //match.InitializeBoard();
+
+            //match.DefinePlayer(); //definir nome do jogador/cor peças
+
+            var matchId = Guid.NewGuid();// _repository.Save(match); 
+            //outra alternativa eh recuperar identificador natural, como o nome dos jogadores do jogo
+            //e sempre que for fazer um movimento, utiliza-los para recuperar o jogo 
+
+            return matchId;
+        }
+
+        public void DoMove(string command, Guid matchId) { 
+        
+            //command like "e2e4" notacao completa ou abreviada
+
+            //var match = _repository.Get(matchId);
+            //match.Move(command);
+
+            //retornar de alguma forma - ilegal move, move ok, etc
+        }
+    }
+}
---
+++ src/Chess.Domain/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Domain
+{
+    public class Match
+    {
+        public Board Board {get;set;}
+        public Player Player{ get; set; } 
+        public IList<Move> Moves { get; set; }
+    }
+    public class Board
+    {
+        public IList<Square> Squares { get; set; }
+        public IList<Piece> Pieces { get; set; }
+    }
+    public class Player
+    {
+    }
+    public class Move
+    {
+    }
+    public class Square
+    {
+    }
+    public abstract class Piece
+    {
+    }
+    public class King : Piece
+    {
+    }
+    public class Queen : Piece
+    {
+    }
+    public class Rook:Piece
+    {
+    }
+    public class Bishop: Piece
+    {
+    }
+    public class Knight : Piece
+    {
+    }
+    public class Pawn:Piece
+    {
+    }
+}
